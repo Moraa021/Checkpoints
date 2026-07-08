9c771d43cebd3cb5a122a807c57c128eae41b0c3
memoraa <mercymoraa012@gmail.com>
2026-07-08 14:52:31 +0300

    go module

---
+++ repeatalpha.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func RepeatAlpha(s string) string {
+	count:=1
+	result:=""
+
+	for _,r:=range s{
+		
+		if r< 'a' && r > 'z' ||r <'A' && r > 'Z'{
+			return s
+		}
+		if r>='a' && r<='z'{
+			count=int((r-'a')+1)
+		}
+		if r>='A' && r<='Z'{
+			count=int((r-'A')+1)
+	}
+
+	for i:=0;i<count;i++{
+		result=result+string(r)
+	}
+}
+	return result
+
+}
+
+
+func main() {
+	fmt.Println(RepeatAlpha("abc"))
+	fmt.Println(RepeatAlpha("Choumi."))
+	fmt.Println(RepeatAlpha(""))
+	fmt.Println(RepeatAlpha("abacadaba 01!"))
+}
